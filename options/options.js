b676548363c5a2b973d4671483211f328fae6d9f
Crowdbotics <team@crowdbotics.com>
2023-12-15 18:53:32 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "logistics-for-dolov-45034",
+  url: "https://logistics-for-dolov-45034.botics.co",
+  api: "https://logistics-for-dolov-45034.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
